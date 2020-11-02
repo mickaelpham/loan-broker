7c42f154d03f9d8814fe768afe35f4ac4ad2aebf
Mickaël Pham <inbox@mickael.dev>
2020-11-01 21:00:04 -0800

    feat: verify translator response

--- loan-test-client/verify.rb
+++ loan-test-client/verify.rb
@@ -1,3 +1,24 @@
 # frozen_string_literal: true
 
+# https://github.com/ddollar/foreman/wiki/Missing-Output
+$stdout.sync = true
+
 require 'bunny'
+require 'json'
+
+connection = Bunny.new
+connection.start
+
+channel = connection.create_channel
+loan_reply = channel.queue('loanReplyQueue')
+
+begin
+  puts ' [*] Waiting for loan replies'
+
+  loan_reply.subscribe(block: true) do |_delivery_info, _properties, body|
+    payload = JSON.parse(body, symbolize_names: true)
+    puts " [>] Received #{payload}"
+  end
+rescue Interrupt => _e
+  connection.close
+end
